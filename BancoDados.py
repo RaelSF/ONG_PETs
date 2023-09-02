57926730680d716b7f72db9945eea5629ec78197
RaelSF <124424379+RaelSF@users.noreply.github.com>
2023-09-02 12:56:19 -0400

    Delete BancoDados.py

--- BancoDados.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-banco = sqlite3.connect('DataBase.db')
-
-cursor=banco.cursor()
-
-#cursor.execute("CREATE TABLE IF NOT EXISTS cadpessoa (nome text,cpf text,email text,telefone text,rua text,numero text,bairro text,cidade text)")
-
